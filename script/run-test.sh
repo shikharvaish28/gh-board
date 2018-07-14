4b82e679b83339f500d686967d3b6b843f7e1d40
Boxuan Li <liboxuan@connect.hku.hk>
2018-07-14 10:21:43 +0800

    run-test.sh: Verify files exist
    Verify `issues.json` and `recent-issues.json`
    files exist after build.
    
    Closes https://github.com/coala/gh-board/issues/74

--- script/run-test.sh
+++ script/run-test.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
+set -e -x
+
+# Verify files exist
+files=( "issues.json" "recent-issues.json" )
+for file in "${files[@]}"
+do
+  if [[ -f "$file" ]]; then
+    echo "File $file exists."
+  else
+    echo "File $file does not exist."
+    exit 1
+  fi
+done
+
 # Set the flag `--race` to kill all tasks when a task finished with zero.
 $(npm bin)/run-p --race "start-selenium" "serve" "build-and-test"
